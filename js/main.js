49f22c7dad399af9ddf866b56b666973f2ca9a77
Basan <basantaad817@gmail.com>
2021-10-20 18:16:02 +0545

    update

--- js/main.js
+++ js/main.js
@@ -34,10 +34,10 @@ navBar.forEach(function(a){
             }
         }, step);
     }
-    counter("count1", 0, 1287, 3000);
-    counter("count2", 100, 5786, 2500);
-    counter("count3", 0, 1440, 3000);
-    counter("count4", 0, 7110, 3000);
+    counter("count1", 0, 127, 3000);
+    counter("count2", 100, 86, 2500);
+    counter("count3", 0, 140, 3000);
+    counter("count4", 0, 710, 3000);
  });
    
    
